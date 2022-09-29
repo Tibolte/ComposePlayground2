f4d629dae4818c7fcfe99b0aa950c051edafa0ab
Thibault Guegan <thibault.guegan@myscript.com>
2022-09-29 08:23:04 +0200

    profile image card

--- app/src/main/java/fr/northborders/composeplayground2/MainActivity.kt
+++ app/src/main/java/fr/northborders/composeplayground2/MainActivity.kt
@@ -3,41 +3,106 @@ package fr.northborders.composeplayground2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import fr.northborders.composeplayground2.ui.theme.ComposePlayground2Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Thibault")
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(0.5f)
+            ) {
+                ImageCard(
+                    painter = painterResource(id = R.drawable.android_logo),
+                    contentDescription = "Android logo",
+                    title = "Here is an Android logo"
+                )
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Row(
-        modifier = Modifier.background(Color.Green).fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+fun Content(name: String) {
+    Column(
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxHeight(0.5f)
+            .width(600.dp)
+            .border(width = 2.dp, color = Color.Magenta),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(text = name)
         Text(text = "Android")
     }
 }
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(5.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent, Color.Black
+                        ),
+                        startY = 300f
+                    ))
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Greeting(name = "Thibault")
+    ImageCard(
+        painter = painterResource(id = R.drawable.android_logo),
+        contentDescription = "Android logo",
+        title = "Here is an Android logo"
+    )
 }
